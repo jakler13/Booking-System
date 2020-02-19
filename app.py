1ebda1f0b08b95c016773d8786c4e8ebef498816
Stephanie Georgiou <stephanie.georgiou@digitalreasoning.com>
2020-02-19 09:58:56 -0600

    Added sign up and login pages

--- app.py
+++ app.py
@@ -17,17 +17,19 @@
 ######################################################################################
 
 
+# SQLAlchemy handles the database
+from datetime import datetime
+
 # I imported render_template, which combines the app route to a html page created in a different file
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-# SQLAlchemy handles the database
-from datetime import datetime
+
 # datetime allows the date the employee picks to be saved in the
-from configparser import SafeConfigParser
 
 app = Flask(__name__)
 
-# app.config for the database below is set to false so there isn't a message sent everytime thre is a change to the database
+# app.config for the database below is set to false so there isn't a message sent everytime thre is a change to the
+# database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config below is where the database is stored using mysql the first part is the server name.
 # After the : is the server password and following that is the web servers name
@@ -39,21 +41,64 @@ db.app = app
 
 
 # I create a class that will create a database model of all the bookings
-class Record(db.Model):
+class Booking(db.Model):
     # id is the primary key for the table .Column gives it the header in the table
     id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String(50))
     # first name (fname) has a max string length of 50
-    lname = db.Column(db.String(50))
+    fname = db.Column(db.String(50))
     # last name (lname) has a max string length of 50
-    email = db.Column(db.String(50))
+    lname = db.Column(db.String(50))
     # email has a max string length of 50
-    room = db.Column(db.Integer)
+    email = db.Column(db.String(50))
     # room will be saved as an integer
+    room = db.Column(db.Integer)
+    # date has its own data type of DateTime
     date = db.Column(db.DateTime)
 
 
-# date has its own data type of DateTime
+class Members(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(50))
+    lname = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/signupresult', methods=['POST'])
+def signupresult():
+    db.create_all()
+    fname = request.form['fname']
+    lname = request.form['lname']
+    email = request.form['email']
+    password = request.form['password']
+    record = Members(fname=fname, lname=lname, email=email, password=password)
+    db.session.add(record)
+    db.session.commit()
+    result = Members.query.all()
+    return render_template('signupresult.html', fname=fname, lname=lname, email=email, password=password,
+                           result=result)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/loginresult', methods=['POST'])
+def loginresult():
+    # TODO login handling
+    return render_template('loginresult.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 # This is the first app route
 # Nothing follows the '/' which means that this is the landing page once the code is run
@@ -63,7 +108,7 @@ class Record(db.Model):
 def index():
     # using the render_template imported earlier
     # the html page associated with what should be shown is called 'home.html'
-    return render_template('home.html')
+    return render_template('sign.html')
 
 
 # Another app route  is defined with the function about
@@ -105,13 +150,13 @@ def feedback():
     date = datetime.strptime(formDateData, '%Y-%m-%d')  # second parameter is local representation of the string
     # The make is in respect to the records class created earlier
     # The variables and values associated are all passed in to add to a databse table
-    record = Record(fname=fname, lname=lname, email=email, room=room, date=date)
+    record = Booking(fname=fname, lname=lname, email=email, room=room, date=date)
     # db.session starts a new entry and then the commit function adds them
     db.session.add(record)
     db.session.commit()
     # a variable result is created which requests the entire contents of the table
     # hence the query.all()
-    result = Record.query.all()
+    result = Booking.query.all()
     # feedback template is called and variables needed in the feedback template are also passed in
     # result is sent in so the entire tables contents can be referenced
     return render_template('feedback.html', fname=fname, lname=lname, email=email, room=room, date=date, result=result)
